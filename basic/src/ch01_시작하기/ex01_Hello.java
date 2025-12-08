5d7818a3a121a6e4ce90a3652ebb576150e7011b
hoho <leflex0821@gmail.com>
2025-12-08 17:58:11 +0900

    1208

---
+++ basic/src/ch01_시작하기/ex01_Hello.java
@@ -0,0 +1,12 @@
+package ch01_시작하기;
+
+public class ex01_Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ctrl+space: 자동완성
+		// ctrl+F11: 실행
+		System.out.println("Hello, Java");
+	}
+
+}
