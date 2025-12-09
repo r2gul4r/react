7727f00f05bb87dcef574bd4622775de418026b6
hoho <leflex0821@gmail.com>
2025-12-09 18:02:15 +0900

    1209

--- basic/src/ch02_데이터와변수/Ex04_long정수.java
+++ basic/src/ch02_데이터와변수/Ex04_long정수.java
@@ -1,17 +1,24 @@
 package ch02_데이터와변수;
 
 /**
- * 정수의 타입 1. byte 1byte 2. short 2byte 3. int 4byte 4. long 8byte 5. char 2byte
+ * 정수의 타입 
+ * 1. byte 1byte 
+ * 2. short 2byte 
+ * 3. int 4byte 
+ * 4. long 8byte 
+ * 5. char 2byte
  */
 
-public class Ex03_byte정수 {
+public class Ex04_long정수 {
 
 	public static void main(String[] args) {
-		byte var1 = -128;
-		byte var2 = -30;
-		byte var3 = 0;
-		byte var4 = 30;
-		byte var5 = 127;
+		// alt+shift+a: 다중편집상태에서 shift+방향키
+		
+		long var1 = -128;
+		long var2 = -30;
+		long var3 = 0;
+		long var4 = 30;
+		long var5 = 127;
 //		byte var6 = 128; //에러? 허용범위 초과
 		// Type mismatch  
 		
